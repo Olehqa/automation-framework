950fc4975370dd70ebdd7e51e528f7abc833b596
Oleh_G <qa1.oleh@gmail.com>
2020-12-29 02:56:03 +0200

    Base test with driver

---
+++ src/main/java/test/BaseTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    public WebDriver driver;
+    public String baseUrl;
+    public boolean acceptNextAlert = true;
+    public StringBuffer verificationErrors = new StringBuffer();
+    public String browser;
+    public WebDriverWait wait;
+
+    @BeforeClass
+    public void setUP() {
+        browser = System.getProperty("browser");
+
+        System.setProperty("webdriver.chrome.driver",
+                "src" + File.separator + "main" + File.separator + "resources" + File.separator + "chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("start-maximized");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--ignore-certificate-errors");
+        driver = new ChromeDriver(options);
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void driverDown() {
+        driver.close();
+        driver.quit();
+    }
+}
