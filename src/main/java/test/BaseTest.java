045a6a4c5967d651124a7ad293947be50da5a3d8
Oleh_G <qa1.oleh@gmail.com>
2021-01-08 23:37:18 +0200

    Base test with driver

--- src/main/java/test/BaseTest.java
+++ src/main/java/test/BaseTest.java
@@ -7,34 +7,29 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-import java.io.File;
+import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
     public WebDriver driver;
-    public String baseUrl;
-    public boolean acceptNextAlert = true;
-    public StringBuilder verificationErrors = new StringBuilder();
-    public String browser;
     public WebDriverWait webDriverWait;
+    private final ResourceBundle waiterTime = ResourceBundle.getBundle("resources.resoursebundle.waitertime");
+    public long smallTime = Long.parseLong(waiterTime.getString("small_time_out"));
+    public long longTime = Long.parseLong(waiterTime.getString("big_time_out"));
 
     @BeforeClass
     public void setUP() {
-        browser = System.getProperty("browser");
-
-        System.setProperty("webdriver.chrome.driver",
-                "src" + File.separator + "main" + File.separator + "resources" + File.separator + "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver_v88.exe");
         ChromeOptions options = new ChromeOptions();
-
         options.addArguments("start-maximized");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--ignore-certificate-errors");
         driver = new ChromeDriver(options);
         driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        webDriverWait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(longTime, TimeUnit.SECONDS);
+        webDriverWait = new WebDriverWait(driver, smallTime);
     }
 
     @AfterClass(alwaysRun = true)
