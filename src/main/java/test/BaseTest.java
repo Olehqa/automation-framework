bb0113fe75e236f4c4afec2b273a1dd9c5820a88
Oleh_G <qa1.oleh@gmail.com>
2021-02-19 23:16:19 +0200

    refactoring

--- src/main/java/test/BaseTest.java
+++ src/main/java/test/BaseTest.java
@@ -1,39 +1,50 @@
 package test;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
-
-import static helpedfunctions.PropertiesUtils.getProperties;
 
 public class BaseTest {
 
-    public static final Logger LOGGER = LogManager.getLogger(BaseTest.class);
-    public WebDriver driver;
-    public WebDriverWait webDriverWait;
+    //    public static final Logger LOGGER = LogManager.getLogger(BaseTest.class);
+    public static WebDriver driver;
+    public String browser;
+//    public static WebDriverWait webDriverWait;
 
-    public static final long SMALL_TIME = Long.parseLong(getProperties("small_time_out"));
-    public static final long LONG_TIME = Long.parseLong(getProperties("big_time_out"));
+//    public static final long SMALL_TIME = Long.parseLong(getProperties("small_time_out"));
+//    public static final long LONG_TIME = Long.parseLong(getProperties("big_time_out"));
 
-    @BeforeClass
+    @BeforeTest(alwaysRun = true)
     public void setUP() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("chromedriver", getProperties("chromedriver")));
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
+        browser = System.getProperty("browser");
+
+        System.setProperty("webdriver.chrome.driver", "src" + File.separator + "main" + File.separator + "resources" + File.separator + "chromedriver_v87.exe");
+//        System.setProperty("webdriver.chrome.driver", System.getProperty("chromedriver", getProperties("chromedriverdef")));
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("start-maximized");
 //        options.addArguments("--disable-dev-shm-usage");
 //        options.addArguments("--no-sandbox");
 //        options.addArguments("--ignore-certificate-errors");
+//        driver = new ChromeDriver(options);
+//        driver.manage().deleteAllCookies();
+//        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//        webDriverWait = new WebDriverWait(driver, SMALL_TIME);
+
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--ignore-certificate-errors");
         driver = new ChromeDriver(options);
         driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(LONG_TIME, TimeUnit.SECONDS);
-//        webDriverWait = new WebDriverWait(driver, SMALL_TIME);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//        webDriverWait = new WebDriverWait(driver, 10);
     }
 
     @AfterClass(alwaysRun = true)
@@ -42,3 +53,38 @@ public class BaseTest {
         driver.quit();
     }
 }
+
+//    public static WebDriver driver;
+//    public WebDriverWait webDriverWait;
+//    public String browser;
+//    private final ResourceBundle waiterTime = ResourceBundle.getBundle("java.resources.waitertime");
+//    public long smallTime = Long.parseLong(waiterTime.getString("small_time_out"));
+//    public long longTime = Long.parseLong(waiterTime.getString("big_time_out"));
+//todo reveiw base test //
+
+//    @BeforeTest(alwaysRun = true)
+//    public void setUP() {
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        String path = classLoader.getResource("chromedriver_v87.exe").getPath();
+
+//        browser = System.getProperty("browser");
+
+//        System.setProperty("webdriver.chrome.driver", "src" + File.separator + "main" + File.separator + "resources" + File.separator + "chromedriver_v87.exe");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("start-maximized");
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--ignore-certificate-errors");
+//        driver = new ChromeDriver(options);
+//        driver.manage().deleteAllCookies();
+//        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+////        webDriverWait = new WebDriverWait(driver, 10);
+//    }
+
+//    @AfterClass(alwaysRun = true)
+//    public void driverDown() {
+//        driver.close();
+//        driver.quit();
+//    }
+
+
